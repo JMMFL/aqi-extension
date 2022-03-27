0a8ce89d42c617f44ed893222a2e19d4c6fc5854
JMMFL <mehravarj+github@gmail.com>
2022-03-27 16:32:57 -0400

    Added basic functionality for grabbing AQI data.

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -11,20 +11,30 @@ import {
 import { calculateAQI } from '../utils/calculator';
 
 const App: React.FC<{}> = () => {
-  // const [status, setStatus] = useStat("loading");
+  const [city, setCity] = useState<City>();
   const [cities, setCities] = useState<ArrayOfCities>([]);
   const [data, setData] = useState<AirData>();
   const [query, setQuery] = useState('');
 
   useEffect(() => {
-    async function getData(query: string) {
+    async function getCities(query: string) {
       const cities = await fetchCities(query);
       setCities(cities);
       return cities;
     }
 
-    getData(query);
+    getCities(query);
   }, [query]);
+
+  useEffect(() => {
+    async function getAirData(city: City) {
+      const data = await fetchAirData(city);
+      setData(data);
+      return data;
+    }
+
+    getAirData(city);
+  }, [city]);
 
   const submitQuery = (event: FormEvent) => {
     const query = event.target['city-search'].value;
@@ -32,10 +42,19 @@ const App: React.FC<{}> = () => {
     event.preventDefault();
   };
 
-  const submitCity = (event: FormEvent) => {
-    console.log(event);
-    event.preventDefault();
-  };
+  if (data) {
+    console.log(data.components);
+    const aqis = calculateAQI(data);
+    const { o3, pm2_5, pm10 } = aqis;
+
+    return (
+      <>
+        <h1>{`Ozone: ${o3}`}</h1>
+        <h1>{`PM2.5: ${pm2_5}`}</h1>
+        <h1>{`PM10: ${pm10}`}</h1>
+      </>
+    );
+  }
 
   return (
     <>
@@ -43,11 +62,11 @@ const App: React.FC<{}> = () => {
         <label htmlFor="city-search">City Search</label>
         <input id="city-search" type="search" name="city-search" required />
       </form>
-      <form onSubmit={submitCity}>
-        {(cities as any).map((city: City) => (
-          <button>{`${city.name}, ${city.country}, ${city.state}`}</button>
-        ))}
-      </form>
+      {(cities as any).map((city: City) => (
+        <button
+          onClick={() => setCity(city)}
+        >{`${city.name}, ${city.country}, ${city.state}`}</button>
+      ))}
     </>
   );
 };
