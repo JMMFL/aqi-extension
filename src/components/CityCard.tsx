5f2ea90b7057059b00d9d48949dc1fee6beef1a2
JMMFL <mehravarj+github@gmail.com>
2022-04-01 19:02:34 -0400

    Added remove button and event listener for CityCard.

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 
 function CityCard({ city, citiesHook }) {
   const removeCity = () => {
-    // chrome.storage.local.get(['cities'], (response) => {
-    //   const cities = response.cities ?? [];
-    //   console.log(cities);
-    //   const index = cities.indexOf(city);
-    //   cities.splice(index, 1);
-    //   chrome.storage.local.set({ cities });
-    //   citiesHook(cities);
-    // });
+    chrome.storage.local.get(['cities'], ({ cities }) => {
+      const { lat, lon } = city;
+
+      let index = 0;
+      for (const city of cities) {
+        if (lat === city.lat && lon === city.lon) break;
+        index++;
+      }
+
+      cities.splice(index, 1);
+      chrome.storage.local.set({ cities });
+      citiesHook(cities);
+    });
   };
 
   return (
     <>
       <div>
         <h1>{city.name}</h1>
-        {/* <button onClick={removeCity}>Remove</button> */}
+        <button onClick={removeCity}>Remove</button>
       </div>
     </>
   );
