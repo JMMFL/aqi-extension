e753ebc2eb3be99001b9e84cae29aa7d4b15598d
JMMFL <mehravarj+github@gmail.com>
2022-04-01 19:24:23 -0400

    Calculated AQIs and main score for each city.

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchAirData, City, AirData } from '../utils/api';
+import { calculateAQI } from '../utils/calculator';
 
 function CityCard({ city, citiesHook }) {
+  const [data, setData] = useState<AirData>();
+
+  useEffect(() => {
+    async function getAirData(city: City) {
+      const data = await fetchAirData(city);
+      setData(data);
+      return data;
+    }
+
+    getAirData(city);
+  }, []);
+
   const removeCity = () => {
     chrome.storage.local.get(['cities'], ({ cities }) => {
       const { lat, lon } = city;
@@ -17,14 +31,25 @@ function CityCard({ city, citiesHook }) {
     });
   };
 
-  return (
-    <>
+  if (data) {
+    const category = data.main.aqi;
+    const { o3, pm2_5, pm10 } = calculateAQI(data);
+
+    return (
       <div>
         <h1>{city.name}</h1>
+        <h2>Index: {category}</h2>
+        <ul>
+          <li>o3: {o3}</li>
+          <li>pm2_5: {pm2_5}</li>
+          <li>pm10: {pm10}</li>
+        </ul>
         <button onClick={removeCity}>Remove</button>
       </div>
-    </>
-  );
+    );
+  } else {
+    return <h1>Not yet</h1>;
+  }
 }
 
 export { CityCard };
