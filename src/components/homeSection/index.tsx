2ce6c8a7a6f77269575ca8eaa52cfd84fe40e06e
JMMFL <mehravarj+jmmfl@gmail.com>
2022-04-18 17:45:48 -0400

    Added scroll to HomeSection

--- src/components/homeSection/index.tsx
+++ src/components/homeSection/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { CityCard } from '../cityCard';
 import { City } from '../../utils/api';
+import { Container } from './styled';
 
 function HomeSection({ cities, citiesHook }) {
   return (
-    <div id="home-section">
+    <Container id="home-section">
       {(cities as any).map((city: City) => (
         <CityCard city={city} citiesHook={citiesHook} />
       ))}
-    </div>
+    </Container>
   );
 }
 
---
+++ src/components/homeSection/styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 370px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+`;
+
+export { Container };
